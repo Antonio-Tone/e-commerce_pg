5a8b7d8e29b8774cb5faf3b908108df487d71177
Antonio-Tone <Antoniotone815@gmail.com>
2023-05-31 21:58:49 +0200

    kuils

--- script/app.js
+++ script/app.js
@@ -1,38 +1,38 @@
 let Product = [
   {
     image: "/img/Bellville.png",
-    Price: "sellling Price: R 1.5m",
+    Price: "selling Price: R 1.5m",
     Address: "1 Pacadi Str, Kuils River.",
-    Discribtion: "3 bedroom property for sale in Kuils River:",
+    Description: "3 bedroom property for sale in Kuils River:",
     icon1: '/icons/icon_bathroom_blue.svg',
     icon2: '/icons/icon_bed_blue.svg',
     icon3: '/icons/icon_secure_parking_blue.svg',
     paragraph:
-      "  A Lovely 3 Bedroom property for sale In the heart of Kuils River. This property has lots of potential when making some changes and/or deciding to make house your home. Or even starting a family. This is a good purchase to invest in or come into the market. Consists of 3 perfect sized bedrooms, kitchen, living room space, family bathroom and space for parking.Situated close to all major routes.Contact me for further details and appointments.",
+      "  A Lovely 3 Bedroom property for sale In the heart of Kuils River. This property has lots of potential when making some changes and/or deciding to make house your home,which makes it  perfect for you",
 
   },
   {
     image: "/img/Lans downe.png",
-    Price: "sellling Price: R 1.9m",
+    Price: "selling Price: R 1.9m",
     Address: "35 Arthur Str , Kuils River.",
-    Discribtion: "3 bedroom property for sale in Kuils River:",
+    Description: "3 bedroom property for sale in Kuils River:",
     icon1: '/icons/icon_bathroom_blue.svg',
     icon2: '/icons/icon_bed_blue.svg',
     icon3: '/icons/icon_secure_parking_blue.svg',
     paragraph:
-      "  A Lovely 3 Bedroom property for sale In the heart of Kuils River. This property has lots of potential when making some changes and/or deciding to make house your home. Or even starting a family. This is a good purchase to invest in or come into the market. Consists of 3 perfect sized bedrooms, kitchen, living room space, family bathroom and space for parking.Situated close to all major routes.Contact me for further details and appointments.",
+      "  A Lovely 3 Bedroom property for sale In the heart of Kuils River. This property has lots of potential when making some changes and/or deciding to make house your home,which makes it  perfect for you",
     
   },
   {
     image: "/img/istockphoto-1026205392-612x612.jpg",
-    Price: "sellling Price: R 2m",
-    Address: "2 Wattle Str",
-    Discribtion: "3 bedroom property for sale in Kuils River:",
+    Price: "selling Price: R 2m",
+    Address: "2 Wattle Str, Kuils River",
+    Description: "3 bedroom property for sale in Kuils River:",
     icon1: '/icons/icon_bathroom_blue.svg',
     icon2: '/icons/icon_bed_blue.svg',
     icon3: '/icons/icon_secure_parking_blue.svg',
     paragraph:
-      "  A Lovely 3 Bedroom property for sale In the heart of Kuils River. This property has lots of potential when making some changes and/or deciding to make house your home. Or even starting a family. This is a good purchase to invest in or come into the market. Consists of 3 perfect sized bedrooms, kitchen, living room space, family bathroom and space for parking.Situated close to all major routes.Contact me for further details and appointments.",
+      "  A Lovely 3 Bedroom property for sale In the heart of Kuils River. This property has lots of potential when making some changes and/or deciding to make house your home,which makes it  perfect for you",
   }
   
 ];
@@ -48,7 +48,7 @@ Product.forEach((data) => {
       id="fadeIn"
       src="${data.image}" 
       class="img-fluid d-block w-50 mx-auto"
-      alt="Avaliable House"
+      alt="Available House"
       loading="lazy"
     />
     <p class="lead text-center font-italic text-muted selling">${data.Price}</p>
@@ -56,7 +56,7 @@ Product.forEach((data) => {
       id="name"
       class="display-5 text-dark text-center mx-auto my-0 pt-1"
     >
-      1 Pacadi Str, Kuils River.
+      ${data.Address}
     </h5>
     <p class="lead text-center text-dark w-75 mx-auto px-0 px-md-5">
      <ol class=" d-flex justify-content-center justify-content-around">
@@ -66,7 +66,7 @@ Product.forEach((data) => {
      </ol>
     </p>
     <br>
-    <p class="text-center para"> <strong>${data.Discribtion}</strong>
+    <p class="text-center para"> <strong>${data.Description}</strong>
         <br>
         ${data.paragraph}.</p>
   </div>` ;
